d6a96fb8eba3333ec558c78ca986ec755e97242e
Daniel Turlinski <daniel.turlinski@gmail.com>
2017-11-25 00:27:46 +0100

    Add simple js script

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+
+var a = prompt('Please input base of triangle', 'a = ?'),
+    h = prompt('Please input height of triangle', 'h = ?'),
+    triangleArea;
+
+triangleArea = a*h/2;
+alert('Triangle field with base a:' + a +' and height h:'+ h +' is equal to: ' + triangleArea);
+console.log('Triangle field with base a:' + a +' and height h:'+ h +' is equal to: ' + triangleArea);
